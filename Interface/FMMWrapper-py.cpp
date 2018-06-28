d5aaad9f6a593058c0f0c73ce47fcf8c1f0a8e81
Floren <fbusabiaga@gmail.com>
2018-06-27 22:44:34 -0400

    passing ndarray

--- Interface/FMMWrapper-py.cpp
+++ Interface/FMMWrapper-py.cpp
@@ -5,14 +5,15 @@
 #include <math.h>
 #include <vector>
 
+#include "FMM/FMMWrapper.h"
 
 namespace bp = boost::python;
 namespace np = boost::python::numpy;
 
-void FMM(){
+void FMM(np::ndarray trg_coord){
 
-  //std::cout << "Hello from C++" << std::endl;
-  //std::cout << "Goodbye from C++" << std::endl;
+  std::cout << "Hello from C++" << std::endl;
+  std::cout << "Goodbye from C++" << std::endl;
   return;
 }
 
@@ -21,11 +22,12 @@ void FMM(){
 
 BOOST_PYTHON_MODULE(periodic_fmm)
 {
-  using namespace boost::python;
+  // using namespace boost::python;
+  // using namespace boost::python::numpy;
 
   // Initialize numpy
-  // Py_Initialize();
-  // np::initialize();
+  Py_Initialize();
+  np::initialize();
 
   // Definitions
   def("FMM", FMM);
