ce46aaabf52150f7fc88510efdf1c84821af0169
Floren <fbusabiaga@gmail.com>
2018-06-28 16:17:26 -0400

    We can call FMM_Wrapper from python

--- Interface/FMMWrapper-py.cpp
+++ Interface/FMMWrapper-py.cpp
@@ -6,9 +6,13 @@
 #include <vector>
 
 #include "FMM/FMMWrapper.h"
+#include "FMM/MiniFMM.h"
 
 namespace bp = boost::python;
 namespace np = boost::python::numpy;
+
+// typedef struct FMM_Wrapper FMM_Wrapper;
+
 
 void FMM(np::ndarray trg_coord){
 
@@ -20,9 +24,13 @@ void FMM(np::ndarray trg_coord){
 
 
 
+
+
+
+
 BOOST_PYTHON_MODULE(periodic_fmm)
 {
-  // using namespace boost::python;
+  using namespace boost::python;
   // using namespace boost::python::numpy;
 
   // Initialize numpy
@@ -31,4 +39,40 @@ BOOST_PYTHON_MODULE(periodic_fmm)
 
   // Definitions
   def("FMM", FMM);
+
+  // Class
+  // 
+  // enum_<Mini_FMM::PAXIS>("PAXIS")
+  // .value("NONE", Mini_FMM::NONE);
+
+  // Define Mock class
+  enum_<Mini_FMM::PAXIS>("Mini_PAXIS")
+    .value("NONE", Mini_FMM::NONE)
+    .value("PX", Mini_FMM::PX)
+    .value("PY", Mini_FMM::PY)
+    .value("PZ", Mini_FMM::PZ)
+    .value("PXY", Mini_FMM::PXY)
+    .value("PXZ", Mini_FMM::PXZ)
+    .value("PYZ", Mini_FMM::PYZ)
+    .value("PXYZ", Mini_FMM::PXYZ);
+  class_<Mini_FMM>("Mini_FMM", init<int, Mini_FMM::PAXIS>())
+    .def("saludo", &Mini_FMM::saludo);
+
+  // Class FMM_Wrapper
+  enum_<FMM_Wrapper::PAXIS>("PAXIS")
+    .value("NONE", FMM_Wrapper::NONE)
+    .value("PX", FMM_Wrapper::PX)
+    .value("PY", FMM_Wrapper::PY)
+    .value("PZ", FMM_Wrapper::PZ)
+    .value("PXY", FMM_Wrapper::PXY)
+    .value("PXZ", FMM_Wrapper::PXZ)
+    .value("PYZ", FMM_Wrapper::PYZ)
+    .value("PXYZ", FMM_Wrapper::PXYZ);
+  // FMM_Wrapper(int mult_order = 10, int max_pts = 2000, int init_depth = 0, PAXIS pbc_ = PAXIS::NONE);
+  class_<FMM_Wrapper>("FMM_Wrapper", init<int, int, int, FMM_Wrapper::PAXIS>());
+  // class_<FMM_Wrapper>("FMM_Wrapper");
+
+  
 }
+
+
--- ModuleTest/Python/example.py
+++ ModuleTest/Python/example.py
@@ -3,9 +3,9 @@ import numpy as np
 import sys
 try:
     from mpi4py import MPI
+    print('MPI imported')
 except ImportError:
     print('It didn\'t find mpi4py!')
-
 
 import periodic_fmm as fmm
 
@@ -15,7 +15,40 @@ if __name__ == '__main__':
     ntrg = 10
     trg_coord = np.random.randn(ntrg, 3)
 
-    fmm.FMM(trg_coord)
+
+    if True:
+        # Try basic function
+        print('\n\n')
+        fmm.FMM(trg_coord)
+        
+    if True:
+        # Try Mini_FMM
+        print('\n\n')
+        print('xxx = ', fmm.Mini_PAXIS.PX)
+        Mini_FMM = fmm.Mini_FMM(10, fmm.Mini_PAXIS.NONE)
+        Mini_FMM.saludo()
+
+    if False:
+        # Test MPI
+        print('\n\n')
+        comm = MPI.COMM_WORLD
+        nprocs = comm.Get_size()
+        rank   = comm.Get_rank()
+
+        if rank == 0:
+            print('rank = ', rank)
+            data = 'Hello!'
+            comm.send(data, dest=nprocs-1, tag=1)
+        elif rank == nprocs-1:
+            data = comm.recv(source=0, tag=1)
+            print('rank ', rank, ' received ', data)
+
+
+    if True:
+        # Try FMM_Wrapper
+        print('\n\n')
+        myFMM = fmm.FMM_Wrapper(10, 1024, 0, fmm.PAXIS.NONE)
+
 
 
     print('# End')
