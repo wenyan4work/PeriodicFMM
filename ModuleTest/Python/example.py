6f94f8ed407fe07755951eacc27e59226e776aa3
Floren <fbusabiaga@gmail.com>
2018-06-29 16:00:36 -0400

    change box size in example

--- ModuleTest/Python/example.py
+++ ModuleTest/Python/example.py
@@ -16,42 +16,28 @@ if __name__ == '__main__':
     mult_order = 10
     max_pts = 1024
     init_depth = 0
-    pbc = fmm.FMM_PAXIS.NONE
 
     # Get MPI parameters
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
     # Create sources and targets
-    nsrc = 1
+    nsrc = 1024
     src_coord = np.random.rand(nsrc, 3)
     src_coord[:,2] *= 0.1
     src_value = np.random.rand(nsrc, 3)
-    ntrg = 2
+    ntrg = 1024
     trg_coord = np.random.rand(ntrg, 3)
     trg_coord[:,2] *= 0.1
     trg_value = np.zeros((ntrg, 3))
     
-    # Set box dimensions
-    box = np.zeros((2,3))
-    m = np.minimum(src_coord[:,0].min(), trg_coord[:,0].min()) 
-    M = np.maximum(src_coord[:,0].max(), trg_coord[:,0].max()) 
-    box[0,0] = m - (M-m) * 0.1
-    box[1,0] = M + (M-m) * 0.1
-    m = np.minimum(src_coord[:,1].min(), trg_coord[:,1].min()) 
-    M = np.maximum(src_coord[:,1].max(), trg_coord[:,1].max()) 
-    box[0,1] = m - (M-m) * 0.1
-    box[1,1] = M + (M-m) * 0.1
-    m = np.minimum(src_coord[:,2].min(), trg_coord[:,2].min()) 
-    M = np.maximum(src_coord[:,2].max(), trg_coord[:,2].max()) 
-    box[0,2] = m - (M-m) * 0.1
-    box[1,2] = M + (M-m) * 0.1
     sys.stdout.flush()
     comm.Barrier()
 
     # Try FMM_Wrapper
+    pbc = fmm.FMM_PAXIS.NONE
     myFMM = fmm.FMM_Wrapper(mult_order, max_pts, init_depth, pbc)
-    fmm.FMM_SetBox(myFMM, box[0,0], box[1,0], box[0,1], box[1,1], box[0,2], box[1,2])
+    fmm.FMM_SetBox(myFMM, -1.0, 1.0, -1.0, 1.0, 0.0, 2.0)
     fmm.FMM_UpdateTree(myFMM, trg_coord, src_coord)
     fmm.FMM_Evaluate(myFMM, trg_value, src_value)
     fmm.FMM_DataClear(myFMM)
@@ -62,7 +48,7 @@ if __name__ == '__main__':
     # Try FMM_WrapperWall2D
     pbc = fmm.FMMWall2D_PAXIS.NONE
     myFMM = fmm.FMM_WrapperWall2D(mult_order, max_pts, init_depth, pbc)
-    fmm.FMMWall2D_SetBox(myFMM, box[0,0], box[1,0], box[0,1], box[1,1], box[0,2], box[1,2])
+    fmm.FMMWall2D_SetBox(myFMM, -2.0, 2.0, -2.0, 2.0, 0.0, 1.0)
     fmm.FMMWall2D_UpdateTree(myFMM, trg_coord, src_coord)
     fmm.FMMWall2D_Evaluate(myFMM, trg_value, src_value)
     fmm.FMMWall2D_DataClear(myFMM)
