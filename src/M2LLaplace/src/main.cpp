369b583a9faa5f2a9091ecc0ee6d617668f76720
Robert Blackwell <rblackwell@flatironinstitute.org>
2019-10-18 08:59:48 -0400

    Add elementary option handling to M2L runtimes

--- src/M2LLaplace/src/main.cpp
+++ src/M2LLaplace/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 namespace Laplace1D3D {
 int main(int argc, char *argv[]);
@@ -21,28 +22,53 @@ int main(int argc, char *argv[]);
 }
 
 int main(int argc, char *argv[]) {
-  int dim = atoi(argv[1]);
+  if (argc != 4) {
+      std::cerr << "Input -[cd] {dim} {N}\n";
+    return 1;
+  }
 
-  switch (dim) {
-  case 1:
-    Laplace1D3DDipole::main(argc - 1, argv + 1);
-    break;
-  case 2:
-    Laplace2D3DDipole::main(argc - 1, argv + 1);
-    break;
-  case 3:
-    Laplace1D3DDipole::main(argc - 1, argv + 1);
-    break;
-  case 4:
-    Laplace1D3D::main(argc - 1, argv + 1);
-    break;
-  case 5:
-    Laplace2D3D::main(argc - 1, argv + 1);
-    break;
-  default:
-      std::cerr << "Invalid dimension " << dim << std::endl;
-      break;
+  bool dipoleFlag = false;
+  if (std::string(argv[1]) == "-d") {
+    dipoleFlag = true;
+  }
+  else if (std::string(argv[1]) == "-c") {
+  }
+  else {
+    std::cerr << "Invalid flag " << argv[1] << ".\n";
+    return 1;
   }
   
+  int dim = atoi(argv[2]);
+  argc -= 2;
+  argv += 2;
+
+  if (dipoleFlag) {
+    switch (dim) {
+    case 1:
+      Laplace1D3DDipole::main(argc, argv);
+      break;
+    case 2:
+      Laplace2D3DDipole::main(argc, argv);
+      break;
+    case 3:
+      Laplace1D3DDipole::main(argc, argv);
+      break;
+    default:
+      std::cerr << "Dimension {" << dim << "} not supported for dipole.\n";
+      return 1;
+    }
+  } else {
+    switch (dim) {
+    case 1:
+      Laplace1D3D::main(argc, argv);
+      break;
+    case 2:
+      Laplace2D3D::main(argc, argv);
+      break;
+    default:
+      std::cerr << "Dimension {" << dim << "} not supported for charge.\n";
+      return 1;
+    }
+  }
   return 0;
 }
--- src/M2LStokes/src/main.cpp
+++ src/M2LStokes/src/main.cpp
@@ -13,22 +13,28 @@ int main(int argc, char *argv[]);
 }
 
 int main(int argc, char *argv[]) {
-  int dim = atoi(argv[1]);
+  if (argc != 3) {
+    std::cerr << "Input: {dim} {N}.\n";
+    return 1;
+  }
 
-  switch (dim){
+  int dim = atoi(argv[1]);
+  argc--;
+  argv++;
+  switch (dim) {
   case 1:
-    Stokes1D3D::main(argc - 1, argv + 1);
+    Stokes1D3D::main(argc, argv);
     break;
   case 2:
-    Stokes2D3D::main(argc - 1, argv + 1);
+    Stokes2D3D::main(argc, argv);
     break;
   case 3:
-    Stokes3D3D::main(argc - 1, argv + 1);
+    Stokes3D3D::main(argc, argv);
     break;
   default:
-      std::cerr << "Invalid dimension " << dim << std::endl;
-      break;
+    std::cerr << "Invalid dimension {" << dim << "}.\n";
+    return 1;
   }
-  
+
   return 0;
 }
