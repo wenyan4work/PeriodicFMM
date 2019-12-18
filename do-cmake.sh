d677212f0bb2b305c3bc1cc7869641e2d7949972
wenyan4work <wenyan4work@gmail.com>
2019-12-18 16:48:44 -0500

    modify CMakeLists

---
+++ do-cmake.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+export USER_LOCAL=$HOME/local
+export SYSTEM_LOCAL=/usr/local
+
+ccmake \
+  -D CMAKE_CXX_COMPILER=mpicxx \
+  -D CMAKE_C_COMPILER=mpicc \
+  -D Eigen3_DIR="${USER_LOCAL}/share/eigen3/cmake" \
+../src
