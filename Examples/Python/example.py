c2163bb0c1e79fdb0e1506d80c8faca548a91786
wenyan4work <wenyan4work@gmail.com>
2019-12-21 17:25:31 -0500

    reorganize python module

--- Examples/Python/example.py
+++ Examples/Python/example.py
@@ -1,7 +1,5 @@
-from __future__ import division, print_function
 import PyPeriodicFMM as fmm
 import numpy as np
-import sys
 from mpi4py import MPI
 # Get MPI parameters
 comm = MPI.COMM_WORLD
@@ -21,12 +19,11 @@ ntrg = 1024
 trg_coord = np.random.rand(ntrg, 3)
 trg_coord[:, 2] *= 0.1
 trg_value = np.zeros((ntrg, 3))
-sys.stdout.flush()
 
-src_coord=src_coord.flatten()
-trg_coord=trg_coord.flatten()
-src_value=src_value.flatten()
-trg_value=trg_value.flatten()
+src_coord = src_coord.flatten()
+trg_coord = trg_coord.flatten()
+src_value = src_value.flatten()
+trg_value = trg_value.flatten()
 comm.Barrier()
 print('# Points created rank', rank)
 # Try FMM_Wrapper
@@ -42,12 +39,12 @@ myFMM.FMM_Evaluate(trg_value, ntrg, src_value)
 # Try FMM_WrapperWall2D
 pbc = fmm.FMM_WrapperWall2D.PAXIS.NONE
 myFMM = fmm.FMM_WrapperWall2D(mult_order, max_pts, init_depth, pbc)
-myFMM.FMM_SetBox( -2.0, 2.0, -2.0, 2.0, 0.0, 1.0)
-myFMM.FMM_UpdateTree( trg_coord, src_coord)
-myFMM.FMM_Evaluate( trg_value,ntrg, src_value)
+myFMM.FMM_SetBox(-2.0, 2.0, -2.0, 2.0, 0.0, 1.0)
+myFMM.FMM_UpdateTree(trg_coord, src_coord)
+myFMM.FMM_Evaluate(trg_value, ntrg, src_value)
 myFMM.FMM_DataClear()
 myFMM.FMM_TreeClear()
-myFMM.FMM_UpdateTree( trg_coord, src_coord)
-myFMM.FMM_Evaluate( trg_value,ntrg, src_value)
+myFMM.FMM_UpdateTree(trg_coord, src_coord)
+myFMM.FMM_Evaluate(trg_value, ntrg, src_value)
 comm.Barrier()
 print('# End')
